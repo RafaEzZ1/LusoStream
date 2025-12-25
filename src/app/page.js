bd9f71ac474f62fb07a0ea6512970fb483862645
RafaEzZ1 <rafaezzpt@gmail.com>
2025-12-25 20:23:34 +0000

    melhor design?

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,9 @@
-// src/app/page.js
-import { Suspense } from "react";
-import Navbar from "@/components/Navbar";
 import HomeClient from "./HomeClient";
 
-export const dynamic = "force-dynamic";
+export const metadata = {
+  title: "LusoStream - Início",
+};
 
 export default function HomePage() {
-  return (
-    <div className="bg-black min-h-screen text-white">
-      <Navbar />
-      <Suspense fallback={<p className="pt-24 px-6 text-gray-400">A carregar…</p>}>
-        <HomeClient />
-      </Suspense>
-    </div>
-  );
+  return <HomeClient />;
 }
