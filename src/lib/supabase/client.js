9f332cb08d352e333e79e5dbb2a9ecf2b590b19a
RafaEzZ1 <rafaezzpt@gmail.com>
2025-12-25 18:43:05 +0000

    teste

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,8 +1,10 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  )
+  // ESTAMOS A FORÇAR AS CHAVES DIRETAMENTE PARA TESTAR
+  // Se isto funcionar, o problema é 100% das variáveis no Vercel
+  const url = "https://pcdtxlqnkblrdvywuvnm.supabase.co";
+  const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjZHR4bHFua2JscmR2eXd1dm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4Nzg2MTIsImV4cCI6MjA3NjQ1NDYxMn0.KBb_ChU_IxYpiTRntNmQ55GtVCt2jzgFNHnvAHIOebk";
+
+  return createBrowserClient(url, key)
 }
