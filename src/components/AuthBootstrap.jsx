2ce2619e5d4ec51c815bf71f1e36a57c849f587e
RafaEzZ1 <rafaezzpt@gmail.com>
2025-12-25 18:36:26 +0000

    teste dnv

--- src/components/AuthBootstrap.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/AuthBootstrap.jsx
-"use client";
-import { useEffect } from "react";
-// O listener agora é automático no AuthProvider.jsx
-// Este ficheiro pode ficar vazio ou ser removido.
-export default function AuthBootstrap() {
-  return null;
-}
