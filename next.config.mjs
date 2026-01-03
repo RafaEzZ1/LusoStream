c710ddc4bc5d5f1c70dc1937fcf0ddfc88244a5f
RafaEzZ1 <rafaezzpt@gmail.com>
2026-01-03 14:37:37 +0000

    melhorias

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // Isto desliga a otimização da Vercel e resolve o erro do limite
+    unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -10,6 +10,7 @@ export default function VideoCard({ movie, onClick }) {
         src={movie.image}
         alt={movie.title || movie.name}
         className="rounded-lg w-full h-[225px] object-cover"
+        loading="lazy" // Adicionado: Melhora a velocidade do site
       />
       <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black via-transparent p-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         <h3 className="text-white font-bold text-sm">{movie.title || movie.name}</h3>
